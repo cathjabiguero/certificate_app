3917d417f9fb9b0ced09689b63dd4941c4b460f1
cathjabiguero30 <catherine.jabiguero@quipper.com>
2018-05-08 15:47:03 +0800

    added Wireframe of certificate in view

--- app/models/owner.rb
+++ app/models/owner.rb
@@ -1,6 +1,5 @@
 class Owner < ApplicationRecord
     validates :header, :title, :subtitle, :recipient_name, :first_paragraph, :second_paragraph, :first_assignatory_name, :first_assignatory_position,
                                                             presence: true #validation
-
     dragonfly_accessor :logo                                                        
 end
